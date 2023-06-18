d0ec3d02365ffa598aa5c6c2e487fa3d37bc1959
Agung Mahadana <100138244+agungmahadana@users.noreply.github.com>
2023-06-19 01:31:44 +0800

    first commit

---
+++ Backend.py
@@ -0,0 +1,60 @@
+import os
+import numpy as np
+from skimage import io, color, util
+from skimage.feature import graycomatrix, graycoprops
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+# Hitung fitur GLCM
+def compute_glcm(image, angles):
+    glcm = graycomatrix(image, distances=[1], angles=angles, levels=256, symmetric=True, normed=True)
+    return glcm
+
+# Hitung matriks GLCM
+def glcm_matrix(image):
+    matrix = []
+    angles = [0, np.pi/4, np.pi/2, 3*np.pi/4, np.pi]
+    metric_texture = ['dissimilarity', 'correlation', 'homogeneity', 'contrast', 'ASM', 'energy']
+    for i in metric_texture:
+        row = []
+        for j in angles:
+            row.append(graycoprops(compute_glcm(image, [j]), prop=i)[0][0])
+        matrix.append(row)
+    return np.array(matrix).flatten()
+
+X = []  # Features
+y = []  # Labels
+
+# Load citra dengan ekspresi happy
+positive_images = os.listdir("dataset/happy/")
+for img_path in positive_images:
+    image = io.imread("dataset/happy/" + img_path)
+    features = glcm_matrix(image)
+    X.append(features)
+    y.append(1)  # Sentimen positif
+
+# Load citra dengan ekspresi sad
+negative_images = os.listdir("dataset/sad/")
+for img_path in negative_images:
+    image = io.imread("dataset/sad/" + img_path)
+    features = glcm_matrix(image)
+    X.append(features)
+    y.append(0)  # Sentimen negatif
+
+# Pisahkan data menjadi data latih dan data uji
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+def knn(features, n):
+    # Inisialisasi model KNN
+    knn = KNeighborsClassifier(n_neighbors=n)
+
+    # Latih model
+    knn.fit(X_train, y_train)
+
+    # Prediksi sentimen
+    prediction = knn.predict([features])
+    
+    # Evaluasi model
+    accuracy = knn.score(X_test, y_test)
+    accuracy_percentage = accuracy * 100
+    return prediction[0], accuracy_percentage
---
+++ Home.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+from skimage import io, color, util, transform
+import Backend as be
+
+st.set_page_config(page_title="Image Processing", page_icon="🖼️")
+st.title("Welcome to IEIS! 👋")
+st.caption("Image Emotion Identification System (IEIS) is a web-based application that can analyze the colors and textures of images based on various techniques. This app allows users to analyze more than 4,000 image samples for color analysis (such as converting an image to a pixel matrix, generating a color histogram, and calculating first-order statistics) and texture analysis (such as generating the Gray Level Co-occurrence Matrix (GLCM) of an image and creating a texture histogram). This app was created by a [student](https://github.com/agungmahadana/) using Python and Streamlit. PASTIKAN GAMBAR YANG KAMU UPLOAD MEMILIKI RASIO 1:1 DAN MEMILIKI RESOLUSI 48x48 DAN TEPAT DI DEPAN WAJAHNYA.")
+
+uploaded_file = st.file_uploader("Choose a file", type=['png', 'jpg', 'jpeg'])
+k = st.number_input("Enter the value of k", min_value=1)
+
+if uploaded_file is None:
+    button = st.button("Predict", disabled=True)
+
+else:
+    button = st.button("Predict", disabled=False)
+    if button:
+        image = io.imread(uploaded_file)
+        
+        if (image.shape[0] > 48) or (image.shape[1] > 48):
+            image = transform.resize(image, (48, 48))
+
+        img = image
+
+        if len(img.shape) == 3:  # Periksa jika gambar memiliki saluran warna
+            img = img[:, :, :3]  # Hapus saluran Alpha
+            img = color.rgb2gray(img)
+
+        img = util.img_as_ubyte(img)
+        new_features = be.glcm_matrix(img)
+        prediction = be.knn(new_features, k)
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(image, caption=uploaded_file.name, use_column_width=True)
+
+        with col2:
+            sentiment = "Happy 😊" if prediction[0] == 1 else "Sad 😔"
+            data_df1 = pd.DataFrame({"sentiment": [sentiment]})
+            data_df2 = pd.DataFrame({"accuracy": [prediction[1]]})
+            st.data_editor(
+                data_df1,
+                column_config={
+                    "sentiment": st.column_config.TextColumn(
+                        "Sentiment",
+                        help="The sentiment of the image",
+                        width="medium",
+                    )
+                },
+                hide_index=True,
+            )
+            st.data_editor(
+                data_df2,
+                column_config={
+                    "accuracy": st.column_config.ProgressColumn(
+                        label="Accuracy",
+                        width="medium",
+                        help="The accuracy of the model",
+                        format="%.2f%%",
+                        min_value=0,
+                        max_value=100,
+                    ),
+                },
+                hide_index=True,
+            )
